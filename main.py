e6c16caf395f66959f5c751be35cb04691834c6e
kamil-korwiel <kamilkorwiel@gmail.com>
2024-10-30 14:07:53 +0100

    second playing with codegen and assert

--- main.py
+++ main.py
@@ -2,25 +2,7 @@ import nest_asyncio; nest_asyncio.apply()  # This is needed to use sync API in r
 from playwright.sync_api import sync_playwright
 
 def main():
-    with sync_playwright() as pw:
-    # create browser instance
-        browser = pw.chromium.launch(
-            # we can choose either a Headful (With GUI) or Headless mode:
-            headless=False,
-        )
-        # create context
-        # using context we can define page properties like viewport dimensions
-        context = browser.new_context(
-            # most common desktop viewport is 1920x1080
-            viewport={"width": 1920, "height": 1080}
-        )
-        # create page aka browser tab which we'll be using to do everything
-        page = context.new_page()
-        page.goto("https://google.pl/")
-        page.get_by_test_id("lnXdpd")
-        element = page.get_by_test_id("lnXdpd")
-        atr = element.get_attribute('src')
-        print(atr)
+    pass
 
 
 if __name__ == "__main__":
--- test_2_google/test_google.py
+++ test_2_google/test_google.py
@@ -1,8 +1,15 @@
-import re
 from playwright.sync_api import Page, expect
+
+
 
 def test_google_attribute(page: Page):
     page.goto("https://google.pl/")
-    element = page.locator("/html/body/div[1]/div[2]/div/img")
-    name = element.get_attribute("alt")
-    assert "Google" == name
+    page.get_by_label("Wybierz język, pl").click()
+    page.get_by_role("button", name="Odrzuć wszystko").click()
+    
+    atr = page.get_by_role("img", name="Google").get_attribute("height")
+    page.set_viewport_size({"width": 640, "height": 480});
+    assert "92" == atr
+    atr = page.get_by_role("img", name="Google").get_attribute("height")
+    page.set_viewport_size({"width": 1280, "height": 960});
+    assert "92" == atr
