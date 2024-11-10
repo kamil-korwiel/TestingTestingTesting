52449ba9633492ae3ae247076e9859250ac8285d
kamil-korwiel <kamilkorwiel@gmail.com>
2024-11-10 21:01:53 +0100

    testing site saucedeemo part 3 (pice sorting)

--- test_3_saucedemo/test_whole_website.py
+++ test_3_saucedemo/test_whole_website.py
@@ -1,81 +1,121 @@
 from playwright.sync_api import Page, expect
+import re
 
 
 user_name = "standard_user"
 password = "secret_sauce"
 
 
-def login(page: Page,user: str,password: str):
+def login(page: Page, user: str, password: str):
     page.goto("https://www.saucedemo.com/")
     page.locator("[data-test=\"username\"]").click()
     page.locator("[data-test=\"username\"]").fill(user)
     page.locator("[data-test=\"password\"]").click()
     page.locator("[data-test=\"password\"]").fill(password)
     page.locator("[data-test=\"login-button\"]").click()
-    
+
+
+def check_descending_order(item_list: list[str | int]) -> bool:
+    for i in range(0, len(item_list)-1):
+        if item_list[i] < item_list[i+1]:
+            return False
+    return True
+
+
+def check_ascending_order(item_list: list[str | int]) -> bool:
+    for i in range(0, len(item_list)-1):
+        if item_list[i] > item_list[i+1]:
+            return False
+    return True
+
 
 def test_have_title(page: Page):
-    login(page, user_name , password)
+    login(page, user_name, password)
     expect(page).to_have_title("Swag Labs")
-    
+
 
 def test_have_inventory(page: Page):
-    login(page, user_name , password)
+    login(page, user_name, password)
     expect(page.locator("[data-test=\"inventory-list\"]")).to_be_visible()
-    
+
 
 def test_have_correct_numbers_of_items_inventory(page: Page):
-    login(page, user_name , password)
+    login(page, user_name, password)
     list_of_items = page.locator("[data-test=\"inventory-item\"]").all()
     assert 6 == len(list_of_items)
-    
+
 
 def test_correct_item_button_text(page: Page):
-    login(page, user_name , password)
-    expect(page.locator("[data-test=\"add-to-cart-sauce-labs-backpack\"]")).to_contain_text("Add to cart")
-    
+    login(page, user_name, password)
+    expect(page.locator(
+        "[data-test=\"add-to-cart-sauce-labs-backpack\"]")).to_contain_text("Add to cart")
+
 
 def test_correct_item_button_text_after_click(page: Page):
-    login(page, user_name , password)
+    login(page, user_name, password)
     page.locator("[data-test=\"add-to-cart-sauce-labs-backpack\"]").click()
-    expect(page.locator("[data-test=\"remove-sauce-labs-backpack\"]")).to_contain_text("Remove")
-    
+    expect(page.locator(
+        "[data-test=\"remove-sauce-labs-backpack\"]")).to_contain_text("Remove")
+
 
 def test_shop_cart_icon_indicator(page: Page):
-    login(page, user_name , password)
+    login(page, user_name, password)
     page.locator("[data-test=\"add-to-cart-sauce-labs-backpack\"]").click()
-    expect(page.locator("[data-test=\"shopping-cart-link\"]")).to_contain_text("1")
-    
+    expect(page.locator(
+        "[data-test=\"shopping-cart-link\"]")).to_contain_text("1")
 
-def test_is_the_same_image(page: Page):
-    login(page, user_name , password)
-    img_from_grid = page.locator("[data-test=\"item-4-img-link\"]").get_by_role("img").get_attribute("src")
+
+def test_is_the_same_source_image(page: Page):
+    login(page, user_name, password)
+    img_from_grid = page.locator(
+        "[data-test=\"item-4-img-link\"]").get_by_role("img").get_attribute("src")
     page.locator("[data-test=\"item-4-img-link\"]").click()
-    img_from_item = page.locator("[data-test=\"item-sauce-labs-backpack-img\"]").get_attribute("src")
+    img_from_item = page.locator(
+        "[data-test=\"item-sauce-labs-backpack-img\"]").get_attribute("src")
     assert img_from_grid == img_from_item
-    
 
-def test_ascending_order(page: Page):
-    login(page, user_name , password)
-    list_of_items = page.locator("[data-test=\"inventory-list\"]").locator("[data-test=\"inventory-item\"]").all()
+
+def test_ascending_order_items_name(page: Page):
+    login(page, user_name, password)
     page.locator("[data-test=\"product-sort-container\"]").select_option("az")
+    list_of_items = page.locator(
+        "[data-test=\"inventory-list\"]").locator("[data-test=\"inventory-item\"]").all()
+    str_title_list = [item.locator(
+        '[data-test="inventory-item-name"]').text_content() for item in list_of_items]
+    assert check_ascending_order(str_title_list)
 
-    uno_item = list_of_items[0].locator('[data-test="inventory-item-name"]').text_content()
-    for i in range(1, len(list_of_items)):
-        dos_item = list_of_items[i].locator('[data-test="inventory-item-name"]').text_content()
-        print(dos_item)
-        print(uno_item < dos_item)
-        uno_item = dos_item
-    
 
-def test_descending_order(page: Page):
-    login(page, user_name , password)
-    list_of_items = page.locator("[data-test=\"inventory-list\"]").locator("[data-test=\"inventory-item\"]").all()
+def test_descending_order_items_name(page: Page):
+    login(page, user_name, password)
     page.locator("[data-test=\"product-sort-container\"]").select_option("za")
-    
-    uno_item = list_of_items[0].locator('[data-test="inventory-item-name"]').text_content()
-    for i in range(1, len(list_of_items)):
-        dos_item = list_of_items[i].locator('[data-test="inventory-item-name"]').text_content()
-        print(dos_item)
-        print(uno_item > dos_item)
-        uno_item = dos_item
+    list_of_items = page.locator(
+        "[data-test=\"inventory-list\"]").locator("[data-test=\"inventory-item\"]").all()
+    str_title_list = [item.locator(
+        '[data-test="inventory-item-name"]').text_content() for item in list_of_items]
+    assert check_descending_order(str_title_list)
+
+
+def test_ascending_order_price(page: Page):
+    login(page, user_name, password)
+    page.locator(
+        "[data-test=\"product-sort-container\"]").select_option("lohi")
+    list_of_items = page.locator(
+        "[data-test=\"inventory-list\"]").locator("[data-test=\"inventory-item\"]").all()
+    str_price_list = [item.locator(
+        '[class="inventory_item_price"]').text_content() for item in list_of_items]
+    int_price_list = [float(re.search(
+        "[0-9]+.[0-9][0-9]", str_price).group(0)) for str_price in str_price_list]
+    assert check_ascending_order(int_price_list)
+
+
+def test_descending_order_price(page: Page):
+    login(page, user_name, password)
+    page.locator(
+        "[data-test=\"product-sort-container\"]").select_option("hilo")
+    list_of_items = page.locator(
+        "[data-test=\"inventory-list\"]").locator("[data-test=\"inventory-item\"]").all()
+    str_price_list = [item.locator(
+        '[class="inventory_item_price"]').text_content() for item in list_of_items]
+    int_price_list = [float(re.search(
+        "[0-9]+.[0-9][0-9]", str_price).group(0)) for str_price in str_price_list]
+    assert check_descending_order(int_price_list)
