0b054c9b7c9e3358f7a5f0313ee2038c6db5764f
kamil-korwiel <kamilkorwiel@gmail.com>
2024-11-27 13:47:55 +0100

    fix: a problem with file path

--- test_4_spokopolish/test_cookies.py
+++ test_4_spokopolish/test_cookies.py
@@ -1,18 +1,32 @@
 from playwright.sync_api import Page,expect
 import pytest
 import json
+import os
+
+PATH_COOKIE_BEFORE_ACCEPT = "test_4_spokopolish/accept_all/cookie_before.json"
+PATH_COOKIE_BEFORE_REJECT = "test_4_spokopolish/reject_all/cookie_before.json"
+COOKIE_CONSENT_NAME = "cookieyes-consent"
 
 
-def save(name:str,payload_json:str) -> None:
-    with open(f'./{name}.json','w') as file:
+def ensure_that_path_exists(file_path):
+    
+    folder_path = os.path.dirname(file_path)
+    if folder_path and not os.path.isdir(folder_path):
+        os.makedirs(folder_path)
+    
+
+def save(path:str,payload_json:str) -> None:
+    ensure_that_path_exists(path)
+    # full_file_name = os.path.basename(file_path)
+    with open(f'./{path}','w') as file:
         file.write(payload_json)
         
-def get_file(name:str) -> dict:
-    with open(f'./{name}.json', "r") as f:
+def get_file(path:str) -> dict:
+    with open(f'./{path}', "r") as f:
         cookie = json.loads(f.read())
         return cookie
     
-def find_cookie(name:str,  cookies:list[dict]) -> dict:
+def find_cookie(name:str, cookies:list[dict]) -> dict:
     for coo in cookies:
         if coo["name"] == name:
             return coo
@@ -24,7 +38,7 @@ class TestConsentCookieAcceptAll():
     def test_cookie_before(self,page_spokopolish:Page):
         context = page_spokopolish.context
         cookie = context.cookies()
-        save("test_4_spokopolish/accept_all/cookie_before",json.dumps(cookie))
+        save(PATH_COOKIE_BEFORE_ACCEPT,json.dumps(cookie))
     
     def test_privacy_popup(self,page_spokopolish: Page):
         expect(page_spokopolish.get_by_role("heading", name="We value your privacy")).to_be_visible()
@@ -36,8 +50,8 @@ class TestConsentCookieAcceptAll():
     def test_cookies_after(self,page_spokopolish: Page):
         context = page_spokopolish.context
         cookies = context.cookies()
-        after = find_cookie("cookieyes-consent",cookies)
-        before = get_file("test_4_spokopolish/accept_all/cookie_before")
+        after = find_cookie(COOKIE_CONSENT_NAME,cookies)
+        before = get_file(PATH_COOKIE_BEFORE_ACCEPT)
         after != before
         
 
@@ -47,7 +61,7 @@ class TestConsentCookieRejectAll():
     def test_cookie_before(self,page_spokopolish:Page):
         context = page_spokopolish.context
         cookie = context.cookies()
-        save("test_4_spokopolish/reject_all/cookie_before",json.dumps(cookie))
+        save(PATH_COOKIE_BEFORE_REJECT,json.dumps(cookie))
     
     def test_privacy_popup(self,page_spokopolish: Page):
         expect(page_spokopolish.get_by_role("heading", name="We value your privacy")).to_be_visible()
@@ -59,6 +73,6 @@ class TestConsentCookieRejectAll():
     def test_cookies_after(self,page_spokopolish: Page):
         context = page_spokopolish.context
         cookies = context.cookies()
-        after = find_cookie("cookieyes-consent",cookies)
-        before = get_file("test_4_spokopolish/reject_all/cookie_before")
+        after = find_cookie(COOKIE_CONSENT_NAME,cookies)
+        before = get_file(PATH_COOKIE_BEFORE_REJECT)
         after == before
