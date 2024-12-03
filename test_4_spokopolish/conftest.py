bbee097b949881dd074b7c298dcdfdf7743f910d
kamil-korwiel <kamilkorwiel@gmail.com>
2024-12-03 18:20:53 +0100

    test(spokopolish_home_page): correct text in bullets points

--- test_4_spokopolish/conftest.py
+++ test_4_spokopolish/conftest.py
@@ -5,7 +5,7 @@ import pytest
 @pytest.fixture(scope='class',autouse=True)
 def page_spokopolish(browser: Browser):
     context = browser.new_context()
-    context.set_default_timeout(10000)
+    context.set_default_timeout(50000)
     page = context.new_page()
     page.goto("https://spokopolish.pl/")
     yield page
--- test_4_spokopolish/test_home_page.py
+++ test_4_spokopolish/test_home_page.py
@@ -5,6 +5,17 @@ import pytest
 @pytest.mark.usefixtures("page_spokopolish")
 class TestVisibilityOfElements:
     
+    points = {
+        "Own system":"We’re the only Polish school teaching with our own system – based on CEFR",
+        "Own materials":"We create our own materials & lesson plans",
+        "Logical, comprehensive, STEP-BY-STEP grammar explanations":"Complex concepts are stripped apart and fed to you in smaller chunks. As a result, you gradually build your understanding of grammar logically. Therefore, we strengthen your ability to apply grammar in practice",
+        "Instruction in a language you speak":"We teach in English (or Ukrainian, Russian, French or Spanish) on basic levels. Then, gradually shift to teaching in Polish",
+        "Language background recognition":"People have different learning needs depending on their language background. So, we don’t mix Slavic & non-Slavic learners in groups below B1",
+        "Friendly learning environment":"Passionate teachers, who create genuine connections with their students",
+        "Also a NGO":"We’re a people-oriented non-governmental organisation (NGO), as well as a language school",
+    }
+    points.keys()
+    
     def test_nav_bar_elements(self,page_spokopolish: Page):
         expect(page_spokopolish.get_by_role("link", name="Home")).to_be_visible()
         expect(page_spokopolish.get_by_role("link", name="Courses", exact=True)).to_be_visible()
@@ -12,4 +23,10 @@ class TestVisibilityOfElements:
         expect(page_spokopolish.get_by_role("link", name="Blog")).to_be_visible()
         expect(page_spokopolish.get_by_role("link", name="About Us")).to_be_visible()
             
-        
+    def test_bullet_points(self, page_spokopolish: Page):
+        # css selector
+        list_of_points = page_spokopolish.locator('.cmsmasters_row_outer').locator('li').all()
+        for point, title in zip(list_of_points, self.points.keys()):
+            expect(point.locator(".cmsmasters_icon_list_item_title")).to_contain_text(title)
+            expect(point.locator(".cmsmasters_icon_list_item_text")).to_contain_text(self.points[title])
+            
