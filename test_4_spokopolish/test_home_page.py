e3222030da13d9495b9e4984a0757fcc60085797
kamil-korwiel <kamilkorwiel@gmail.com>
2024-12-06 12:59:17 +0100

    test(spokopolish_home_page): cheking profile teachers with site teachers

--- test_4_spokopolish/test_home_page.py
+++ test_4_spokopolish/test_home_page.py
@@ -14,7 +14,6 @@ class TestVisibilityOfElements:
         "Friendly learning environment":"Passionate teachers, who create genuine connections with their students",
         "Also a NGO":"We’re a people-oriented non-governmental organisation (NGO), as well as a language school",
     }
-    points.keys()
     
     def test_nav_bar_elements(self,page_spokopolish: Page):
         expect(page_spokopolish.get_by_role("link", name="Home")).to_be_visible()
@@ -29,4 +28,21 @@ class TestVisibilityOfElements:
         for point, title in zip(list_of_points, self.points.keys()):
             expect(point.locator(".cmsmasters_icon_list_item_title")).to_contain_text(title)
             expect(point.locator(".cmsmasters_icon_list_item_text")).to_contain_text(self.points[title])
-            
+
+    def test_featured_teachers_and_teachers_in_site(self,page_spokopolish: Page):
+        list_of_profiles = page_spokopolish.locator('.cmsmasters_profile').locator('article').all()
+        page_spokopolish.goto('https://spokopolish.pl/teachers/')
+        all_list_of_profiles_in_site = page_spokopolish.locator('.cmsmasters_profile').locator('article').all()
+        
+        def func(profile):
+            name = profile.locator('.entry-title').inner_text()
+            languages = profile.locator('.pl_subtitle').inner_text()
+            return (name,languages)
+        
+        tuples_profile= set(map(func,list_of_profiles))
+        all_tuples_profile= set(map(func,all_list_of_profiles_in_site))
+        
+        assert tuples_profile.issubset(all_tuples_profile)
+        page_spokopolish.go_back()
+        
+        
